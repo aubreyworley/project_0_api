4778545ef00bc5d162f6059a454aa045698dd17a
aubreyworley <aubreyworley@gmail.com>
2015-07-14 00:14:42 -0700

    added mongodb

--- models/story.js
+++ models/story.js
@@ -3,33 +3,12 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var StorySchema = new Schema({
+var PostSchema = new Schema({
   district: String,
   story: String
 });
 
-var Story = mongoose.model('Story', StorySchema);
+var Post = mongoose.model('Post', PostSchema);
 
-module.exports = Story;
+module.exports = Post;
 
-// get all stories
-app.get('/api/stories', function (req, res) {
-  // find all stories in db
-  Story.find(function (err, stories) {
-    res.json(stories);
-  });
-});
-
-// create new story
-app.post('/api/stories', function (req, res) {
-  // create new story with form data (`req.body`)
-  var newStory = new Story({
-    district: req.body.district,
-    story: req.body.story
-  });
-
-  // save new story in db
-  newStory.save(function (err, savedStory) {
-    res.json(savedStory);
-  });
-});
